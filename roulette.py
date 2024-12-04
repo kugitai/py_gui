a1601722256eab50bf8c99ee2efc87e0b20bf847
kugitai <98938330+kugitai@users.noreply.github.com>
2024-12-04 14:01:29 +0900

    あたり時の演出を追加
    リーチは黄色、大当たりは赤に文字の色が変わる

--- roulette.py
+++ roulette.py
@@ -8,7 +8,7 @@ layout = [[sg.Text('スロット')],
 
 window = sg.Window('ルーレット',layout)
 #スロットの動作中か否か（falseは停止、trueは動作）
-start = [False,False,False] 
+button = [False,False,False] 
 #out1~3は初期値(同じ数字にならないようにランダムにしている)
 out1 = random.randint(1,7)
 out2 = random.randint(1,7)
@@ -19,32 +19,44 @@ while True:
     if event == sg.WINDOW_CLOSED:
         break
     if event == 'スタート':
-        start = [True,True,True]
+        button = [True,True,True]
         window['e_word'].update('')
     
-    if start[0]:
+    if button[0]:
         window['out1'].update(random.randint(under_number, 7))
-    if start[1]:
+    
+    if button[1]:
         window['out2'].update(random.randint(under_number, 7))
-    if start[2]:
+    
+    if button[2]:
         window['out3'].update(random.randint(under_number, 7))
+    
 
     if event == 'stop1':
         out1 = int(window['out1'].DisplayText)
-        start[0] = False
+        button[0] = False
     if event == 'stop2':
         out2 = int(window['out2'].DisplayText)
-        start[1] = False
+        button[1] = False
     if event == 'stop3':
         out3 = int(window['out3'].DisplayText)
-        start[2] = False
-    if not any(start):
+        button[2] = False
+    if not any(button):
         if out1 == out2 == out3:
             window['e_word'].update('大当たり！')
+            window['out1'].update(text_color=("red"))
+            window['out2'].update(text_color=("red"))
+            window['out3'].update(text_color=("red"))
         elif out1 == out2 or out1 == out3 or out2 == out3:
             window['e_word'].update('リーチ')
-            under_number = 5
+            under_number = 5 #確率を上げるため１〜７を５〜７にする
+            window['out1'].update(text_color=("yellow"))
+            window['out2'].update(text_color=("yellow"))
+            window['out3'].update(text_color=("yellow"))
         else:
             window['e_word'].update('')
             under_number = 1
+            window['out1'].update(text_color=("white"))
+            window['out2'].update(text_color=("white"))
+            window['out3'].update(text_color=("white"))
     
