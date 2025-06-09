3cc642ade3e538b9e620247f478adca4393a3ed3
kugitai <98938330+kugitai@users.noreply.github.com>
2025-06-09 14:48:45 +0900

    ジャグラー運試し
    運試しできるプログラムを作成した

---
+++ peka.py
@@ -0,0 +1,22 @@
+import PySimpleGUI as sg
+import random
+
+layout = [[sg.Text('どれぐらいでペカルかな？')],
+        [sg.Text(key='result', size=(20, 1))],
+        [sg.Text(key='count', size=(20, 1))],
+        [sg.Button('Push')]]
+
+window = sg.Window('ペカる', layout)
+count = 0
+while True:
+    event, value = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+    if event == 'Push':
+        result = random.randint(1, 100)
+        if result <= 25:  # 10%の確率でペカる
+            window['result'].update('ペカった！')
+        else:
+            window['result'].update('残念、ペカらなかった。')
+        count += 1 
+    window['count'].update(f'試行回数: {count}')
