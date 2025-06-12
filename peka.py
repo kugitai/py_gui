7db8deb5f0559f60783b3206c6323f2d1bd1ac0a
kugitai <98938330+kugitai@users.noreply.github.com>
2025-06-12 10:14:03 +0900

    機能追加
    リセットボタンの追加

--- peka.py
+++ peka.py
@@ -4,7 +4,7 @@ import random
 layout = [[sg.Text('どれぐらいでペカルかな？')],
         [sg.Text(key='result', size=(20, 1))],
         [sg.Text(key='count', size=(20, 1))],
-        [sg.Button('Push')]]
+        [sg.Button('Push')],[sg.Button('Reset')]]
 
 window = sg.Window('ペカる', layout)
 count = 0
@@ -16,6 +16,7 @@ while True:
         result = random.randint(1, 100)
         if result <= 25:  # 10%の確率でペカる
             window['result'].update('ペカった！')
+            count = 0
         else:
             window['result'].update('残念、ペカらなかった。')
         count += 1 
